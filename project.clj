81cefb0964e2fcc74ddb58df985a9a1a655a0f7a
gneissguise <frostjust@gmail.com>
2018-04-08 12:58:13 -0700

    Adding Reagent dependency to project file

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
-                 [compojure "1.6.0"]]
+                 [compojure "1.6.0"]
+                 [reagent "0.8.0-alpha2"]]
   :plugins [[lein-ring "0.12.3":exclusions [org.clojure/clojure]]]
   :main ^:skip-aot guppy-gae.core
   :target-path "target/%s"
